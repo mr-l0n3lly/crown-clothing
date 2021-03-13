faa40f6c764cafaec906dd74adde5a72e1756040
Andrei Pavalachi <andreipatan@live.com>
2021-03-13 23:53:49 +0200

    Updating our app with shop page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Switch } from 'react-router';
 import { Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/Homepage/Homepage';
+import ShopPage from './pages/Shop/Shop';
 
 function App() {
   return (
     <div>
 		<Switch>
-			<Route exact path='/' component={Homepage} />
+			<Route exact path='/' component={ Homepage } />
+			<Route path='/shop' component={ ShopPage } />
 		</Switch>
     </div>
   );
