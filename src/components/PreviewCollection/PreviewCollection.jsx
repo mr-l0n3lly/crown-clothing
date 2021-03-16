3a1eba2c368f6257dc3550f6b1657ac97e1ee6cb
Andrei Pavalachi <andreipatan@live.com>
2021-03-16 12:23:25 +0200

    backup

---
+++ src/components/PreviewCollection/PreviewCollection.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './PreviewCollection.scss';
+import CollectionItem from '../CollectionItem/CollectionItem';
+
+const PreviewCollection = ({ title, items }) => {
+	return (
+		<div className='preview-collection'>
+			<h1 className='title'>{ title.toUpperCase() }</h1>
+			<div className='preview'>
+				{
+					items
+					.filter((item, index) => index < 4)
+					.map(({ id, ...itemProps }) => {
+						return (
+							<CollectionItem key={ id } {...itemProps} />
+						);
+					})
+				}
+			</div>
+		</div>
+	);
+}
+
+export default PreviewCollection;
---
+++ src/pages/Shop/Shop.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import ShopData from './Shop.data.js';
+import PreviewCollection from '../../components/PreviewCollection/PreviewCollection';
+
+class ShopPage extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			collections: ShopData
+		};
+	}
+
+	render() {
+		const { collections } = this.state;
+		return (
+			<div className='shop-page'>
+				{
+					collections.map(({ id, ...otherData }) => {
+						return (
+							<PreviewCollection key={ id } { ...otherData } />
+						);
+					})
+				}
+			</div>
+		);
+	}
+}
+
+export default ShopPage;
