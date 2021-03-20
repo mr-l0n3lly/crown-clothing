c19869d71f7526604911924b0966375818d25ef9
mr-l0n3lly <andreipatan@live.com>
2021-03-20 13:49:11 +0200

    Finished normal signup component

--- src/components/SignUp/SignUp.component.jsx
+++ src/components/SignUp/SignUp.component.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 import Form from '../Form/Form';
-import Button from "../Button/Button";
+import Button from '../Button/Button';
 
-import { auth, createUserProfileDocument } from "../../firebase/firebase.utils";
+import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
 
 import './SignUp.style.scss';
 
@@ -16,21 +16,24 @@ class SignUp extends React.Component {
             email: '',
             password: '',
             confirmPassword: ''
-        }
+        };
     }
 
-    handleSubmit = async (event) => {
+    handleSubmit = async event => {
         event.preventDefault();
 
         const { displayName, email, password, confirmPassword } = this.state;
 
         if (password !== confirmPassword) {
-            alert('Password do not match');
+            alert("passwords don't match");
             return;
         }
 
         try {
-            const { user } = await auth.createUserWithEmailAndPassword(email, password);
+            const { user } = await auth.createUserWithEmailAndPassword(
+                email,
+                password
+            );
 
             await createUserProfileDocument(user, { displayName });
 
@@ -41,56 +44,55 @@ class SignUp extends React.Component {
                 confirmPassword: ''
             });
         } catch (error) {
-            console.log(error);
+            console.error(error);
         }
-    }
+    };
 
     handleChange = event => {
         const { name, value } = event.target;
 
         this.setState({ [name]: value });
-    }
+    };
 
     render() {
         const { displayName, email, password, confirmPassword } = this.state;
         return (
             <div className='sign-up'>
-                <h2 className='title'>I do not have an account</h2>
+                <h2 className='title'>I do not have a account</h2>
                 <span>Sign up with your email and password</span>
-                <form className='sign-up-form' onSubmit={ this.handleSubmit }>
+                <form className='sign-up-form' onSubmit={this.handleSubmit}>
                     <Form
                         type='text'
                         name='displayName'
-                        value={ displayName }
-                        onChange={ this.handleChange }
-                        label='Display name'
+                        value={displayName}
+                        onChange={this.handleChange}
+                        label='Display Name'
                         required
                     />
                     <Form
                         type='email'
                         name='email'
-                        value={ email }
-                        onChange={ this.handleChange }
+                        value={email}
+                        onChange={this.handleChange}
                         label='Email'
                         required
                     />
                     <Form
                         type='password'
-                        name='passwd'
-                        value={ password }
-                        onChange={ this.handleChange }
+                        name='password'
+                        value={password}
+                        onChange={this.handleChange}
                         label='Password'
                         required
                     />
                     <Form
                         type='password'
-                        name='passwdConfirm'
-                        value={ confirmPassword }
-                        onChange={ this.handleChange }
-                        label='Confirm password'
+                        name='confirmPassword'
+                        value={confirmPassword}
+                        onChange={this.handleChange}
+                        label='Confirm Password'
                         required
                     />
-
                     <Button type='submit'>SIGN UP</Button>
                 </form>
             </div>
