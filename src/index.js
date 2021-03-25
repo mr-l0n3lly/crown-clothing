e4593ea58ffd33ff9eb5c60ab6a9a7b969f96afd
Andrei Pavalachi <andreipatan@live.com>
2021-03-25 19:47:17 +0200

    Added redux and root-reducer + storage + user reducer + user actions

--- src/index.js
+++ src/index.js
@@ -3,15 +3,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './redux/store';
+
+// Redux
 
 ReactDOM.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</React.StrictMode>,
+	<Provider store={store}>
+		<React.StrictMode>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</React.StrictMode>
+	</Provider>,
 	// eslint-disable-next-line no-undef
 	document.getElementById('root')
 );
---
+++ src/redux/user/user.actions.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line import/prefer-default-export
+export const setCurrentUser = (user) => ({
+	type: 'SET_CURRENT_USER',
+	payload: user,
+});
