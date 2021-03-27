da3600c349507b12d6ccca78c6faa4746ea892ea
mr-l0n3lly <andreipatan@live.com>
2021-03-27 16:20:10 +0200

    Trying to insert map to porps from redux

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { connect } from 'react-redux';
 
 import './Header.scss';
 
@@ -41,4 +42,8 @@ const Header = ({ currentUser }) => (
 	</div>
 );
 
-export default Header;
+const mapStateToProps = (state) => ({
+	currentUser: state.user.currentUser,
+});
+
+export default connect(mapStateToProps)(Header);
