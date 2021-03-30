c3ca94dfed222eff2ee7de890cd53bf5a53a7585
mr-l0n3lly <andreipatan@live.com>
2021-03-30 21:20:19 +0300

    display items in cart dropdown component from redux state

--- src/components/Cart/Cart.component.jsx
+++ src/components/Cart/Cart.component.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
+
+import { connect } from 'react-redux';
+
 import Button from '../Button/Button';
+import CartItem from '../CartItems/CartItem.component';
 
 import './Cart.styles.scss';
 
-const Cart = () => (
+// eslint-disable-next-line no-unused-vars,react/prop-types
+const Cart = ({ cartItems }) => (
 	<div className="cart-dropdown">
-		<div className="cart-items" />
+		<div className="cart-items">
+			{
+				// eslint-disable-next-line react/prop-types,array-callback-return
+				cartItems.map((item) => (
+					<CartItem key={item.id} item={item} />
+				))
+			}
+		</div>
 		<Button>GO TO CHECKOUT</Button>
 	</div>
 );
 
-export default Cart;
+const mapStateToProps = (state) => ({
+	cartItems: state.cart.cartItems,
+});
+
+export default connect(mapStateToProps)(Cart);
