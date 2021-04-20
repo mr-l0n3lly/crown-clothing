a0149073c8cdcc6a87372a5a83e7d2b298c7125f
mr-l0n3lly <andreipatan@live.com>
2021-04-20 15:45:25 +0300

    added remove from cart

--- src/components/CheckoutItem/CheckoutItem.component.jsx
+++ src/components/CheckoutItem/CheckoutItem.component.jsx
@@ -1,18 +1,38 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable react/jsx-no-comment-textnodes */
 import React from 'react';
+
+import { connect } from 'react-redux';
+import { clearItemFromCart } from '../../redux/cart/cart.actions';
 
 import './CheckoutItem.styles.scss';
 
 // eslint-disable-next-line react/prop-types
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => (
-	<div className="checkout-item">
-		<div className="image-container">
-			<img src={imageUrl} alt="item" />
+const CheckoutItem = ({ cartItem, clearItem }) => {
+	// eslint-disable-next-line react/prop-types
+	const { name, imageUrl, price, quantity } = cartItem;
+	return (
+		<div className="checkout-item">
+			<div className="image-container">
+				<img src={imageUrl} alt="item" />
+			</div>
+			<span className="name">{name}</span>
+			<span className="quantity">
+				<div className="arrow">&#10094;</div>
+				<span className="value">{quantity}</span>
+				<div className="arrow">&#10095;</div>
+			</span>
+			<span className="price">{price}</span>
+			<div className="remove-button" onClick={() => clearItem(cartItem)}>
+				&#10006;
+			</div>
 		</div>
-		<span className="name">{name}</span>
-		<span className="quantity">{quantity}</span>
-		<span className="price">{price}</span>
-		<div className="remove-button">&#10006;</div>
-	</div>
-);
+	);
+};
 
-export default CheckoutItem;
+const mapDispatchToProps = (dispatch) => ({
+	clearItem: (item) => dispatch(clearItemFromCart(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
